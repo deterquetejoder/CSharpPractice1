ad6d73d2edb9c454ba389eaaf22d9ee01d819d6b
deterquetejoder <saecularisator@gmail.com>
2023-07-22 13:25:07 +0500

    casually solved

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Type a first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Type a second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Type a third number: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = 0;
+
+if(max<a)
+{
+   max = a;
+}
+if(max<b)
+{
+    max = b;
+}
+if(max<c)
+{
+    max = c;
+}    
+
+Console.WriteLine("max = " + max);
