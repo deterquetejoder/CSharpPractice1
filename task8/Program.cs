2efdd7d193268818d1f060b3992ad267020108c1
deterquetejoder <saecularisator@gmail.com>
2023-07-22 14:42:42 +0500

    ts8 solved

--- task8/Program.cs
+++ task8/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿Console.WriteLine("Insert number N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 0 ;
+
+while(i < n)
+{
+    if(i % 2 == 0)
+    {
+        Console.WriteLine(i);
+        i++ ;
+    }
+    else
+    {
+        i++ ;
+    }
+}
