3babd0fb593459584ecd25eb00bcc5c4dbf26971
deterquetejoder <saecularisator@gmail.com>
2023-07-22 14:26:25 +0500

    ts solved

--- task6/Program.cs
+++ task6/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿Console.WriteLine("We're going to check if your number is even. Type your number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int b = a % 2;
+
+if (b==0)
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no");
+}
